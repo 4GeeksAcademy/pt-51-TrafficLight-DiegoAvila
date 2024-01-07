9190c3736e57030763e203e8e8493a46d337f6d9
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2024-01-07 16:08:32 +0000

    solucion posible

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -4,16 +4,37 @@ function Semaforo({color}) {
 
     const [colorOn,setColorOn]=useState(color)
     console.log(colorOn);
-
+    console.log(color)
     function on() {
-        colorOn === "bg-danger bg-opacity-50" ? setColorOn("bg-danger") : setColorOn("bg-danger bg-opacity-50")
-        colorOn === "bg-warning bg-opacity-50" ? setColorOn("bg-warning") : setColorOn("bg-warning bg-opacity-50")
-        colorOn === "bg-success bg-opacity-50" ? setColorOn("bg-success") : setColorOn("bg-success bg-opacity-50")    
-        
-    }
+        // he intentado hacaerlo de esta forma pero hay algo que no hago bien
 
+        // colorOn === "bg-danger bg-opacity-50" ? setColorOn("bg-danger") : setColorOn("bg-danger bg-opacity-50")
+        // colorOn === "bg-warning bg-opacity-50" ? setColorOn("bg-warning") : setColorOn("bg-warning bg-opacity-50")
+        // colorOn === "bg-success bg-opacity-50" ? setColorOn("bg-success") : setColorOn("bg-success bg-opacity-50")
+        
+        // esta otra forma de acerlo seguro que hay mejor manera de programarlo pero no doy con la solucion
+        if (colorOn === "bg-danger bg-opacity-50"){
+            setColorOn("bg-danger")
+        }
+        if (colorOn === "bg-warning bg-opacity-50"){
+            setColorOn("bg-warning")
+        }
+        if (colorOn === "bg-success bg-opacity-50"){
+            setColorOn("bg-success")
+        }
+        if (colorOn === "bg-danger"){
+            setColorOn("bg-danger bg-opacity-50")
+        }
+        if (colorOn === "bg-warning"){
+            setColorOn("bg-warning bg-opacity-50")
+        }
+        if (colorOn === "bg-success"){
+            setColorOn("bg-success bg-opacity-50")
+        }
+    }
+    
     return(
-        <button type="button" onClick={on} className={`btn ${color} rounded-circle m-3`} style={{width: "120px", height: "120px"}}></button>
+        <button type="button" onClick={on} className={`btn ${colorOn} rounded-circle m-3`} style={{width: "120px", height: "120px"}}></button>
     );
     
 }
